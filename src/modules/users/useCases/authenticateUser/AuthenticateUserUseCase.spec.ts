86ccb19143eee17b678d147ee26185cb5033c474
Vyctor <dev.vyctor@gmail.com>
2021-08-15 21:37:43 -0300

    implementando teste unitario para autenticacao de usuario

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { User } from "../../entities/User";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+let user: User;
+
+describe("Authenticate User", () => {
+  beforeEach(async () => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      inMemoryUsersRepository
+    );
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to authenticate a user", async () => {
+    const user = {
+      name: "Vyctor",
+      email: "dev.vyctor@gmail.com",
+      password: "123123",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const authentication = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(authentication).toHaveProperty("token");
+  });
+});
